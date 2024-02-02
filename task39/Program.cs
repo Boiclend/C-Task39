253ecd5e2018125a52b53fca0fff2ea7f6cfe147
Roman <igoshin1922@gmail.com>
2024-02-02 14:21:29 +0500

    added task39

--- task39/Program.cs
+++ task39/Program.cs
@@ -3,9 +3,10 @@
 // Текст должен содержать не более 1000 символов. Вывод должен быть в UPPER CASE (верхний регистр).
 // Написать функцию void mostRecent(char *text,char *word).
 
-
 Console.WriteLine("Введите строку: ");
 string text = Console.ReadLine();
+int count = 0;
+int temp = 0;
 MostRecent(text);
 
 void MostRecent(string text)
@@ -26,15 +27,32 @@ foreach (string word in words)
     }
 }
 
-var mostFrequent = wordFrequency.OrderByDescending(pair => pair.Value).FirstOrDefault();
+foreach (var item in wordFrequency)
+{
+    var mostFrequent = wordFrequency.OrderByDescending(pair => pair.Value).FirstOrDefault();
 
-if (mostFrequent.Value == 1)
-{
-    Console.WriteLine("Не одно слово не встречается чаще других");
-}
-else
-{
-    Console.WriteLine("Самое часто встречающееся слово: " + mostFrequent.Key.ToUpperInvariant());
-    Console.WriteLine("Число повторений в тексте: " + mostFrequent.Value);
+
+    if (mostFrequent.Value == 1 && count == 0)
+    {
+        Console.WriteLine("Не одно слово не встречается чаще других");
+        return;
+    }
+    else
+    {
+    if(count > 0 && temp == mostFrequent.Value)
+    {
+        Console.WriteLine("Самое часто встречающееся слово: " + mostFrequent.Key.ToUpperInvariant());
+        Console.WriteLine("Число повторений в тексте: " + mostFrequent.Value);
+    }
+    if(count == 0)
+    {
+        Console.WriteLine("Самое часто встречающееся слово: " + mostFrequent.Key.ToUpperInvariant());
+        Console.WriteLine("Число повторений в тексте: " + mostFrequent.Value);
+        temp = mostFrequent.Value;
+        count++;
+    }
+        
+    }
+    wordFrequency.Remove(mostFrequent.Key);
 }
 }
